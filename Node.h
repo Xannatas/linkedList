a4dfc83397cfdf676be62dd9b6994748f1b5aca9
banta-isaiah <bantaib@whitman.edu>
2017-11-25 00:32:46 -0800

    Update Node.h

--- Node.h
+++ Node.h
@@ -8,14 +8,24 @@ class Node {
 
     // 2 arg constructor, sets char, and next
     Node(char, Node*);
-
+    
+    //Get data of given node.
     char get_data();
+    
+    //Set data of given node, takes a character arg.
     void set_data(char);
+    
+    //Get data of the node after the given node, takes Node* arg.
     Node* get_next();
+  
+    //Set data of the node after the given node, takes Node* arg.
     void set_next(Node*);
 
   private:
+    //Value of the character c
     char c;
+    
+    //Value of the next node
     Node* next;
 
 };
